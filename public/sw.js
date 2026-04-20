0b55bdcfc338f10a12f980b5fd6e0f713dd9a161
Felipe Desidério <felipe.desiderio@peers.com.br>
2026-04-20 13:49:41 -0300

    Convert site to PWA (App)

---
+++ public/sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'sdm-links-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700;800&family=Archivo+Black&family=Libre+Caslon+Display&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Suporte inicial para notificações
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : { title: 'Drope Novo!', body: 'Confira os últimos tênis no SDM Links.' };
+  const options = {
+    body: data.body,
+    icon: '/icons/icon-192.png',
+    badge: '/icons/icon-192.png',
+    vibrate: [100, 50, 100],
+    data: {
+      url: data.url || '/'
+    }
+  };
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
